2ab53868c3cb7c4ec1e8cdbc8263d813aff7cdb7
tj <tj@durable.ai>
2022-10-10 12:16:58 -0600

    Bump notion client for bugfix

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_description():
 
 setup(
     name="notion-client",
-    version="1.0.0",
+    version="1.0.1",
     url="https://github.com/ramnes/notion-sdk-py",
     author="Guillaume Gelin",
     author_email="contact@ramnes.eu",
